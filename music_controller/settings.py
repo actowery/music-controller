519d600973a3561f0ce90b9c35fec849bb73629c
Adrian <adrian.towery@gmail.com>
2022-09-25 16:36:25 +0000

    rotate DJANGO_SECRET_KEY and use env instead

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(i$5rcjl9=l&vi&(!515h39ij#!xmfqj()en)w14s^%2$)y!4y'
+# Set DJANGO_SECRET_KEY in env before running the server
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
